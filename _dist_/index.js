9638fef391057c488a4b7c786ff4e7b316d2f240
Oen844 <Oen844@users.noreply.github.com>
2022-04-18 16:38:38 +0000

    deploy: 52afb1972a1b786e515196bf1d617ceb417b0aab

--- _dist_/index.js
+++ _dist_/index.js
@@ -3,4 +3,33 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-console.log('Happy hacking :)')
+console.log("Happy hacking :) -----------)");
+const url = "https://platzi-avo.vercel.app/api/avo";
+const base_url = "https://platzi-avo.vercel.app/";
+
+const appNode = document.querySelector('#app');
+
+window
+  .fetch(url)
+  .then((response) => response.json())
+  .then((responseJson) => {
+    const allItems = [];
+    responseJson.data.forEach((item) => {
+      //crear imagen
+      const image = document.createElement("img");
+      image.src = base_url + item.image;
+      //crear titulo
+      const title = document.createElement("h2");
+      title.textContent = item.name;
+      //crear preio
+      const price = document.createElement("div");
+      price.textContent = item.price;
+
+      const container = document.createElement("div");
+
+      container.append(image, title, price);
+
+      allItems.push(container);
+    });
+    appNode.append(...allItems);
+  });
