e62c3f11546a4ca7207d80f7c3076b6a5d73f0e3
Pau Egea <papau84@gmail.com>
2022-04-19 15:04:00 +0200

    internacional $

--- src/index.js
+++ src/index.js
@@ -7,7 +7,16 @@ console.log("Happy hacking :) -----------)");
 const url = "https://platzi-avo.vercel.app/api/avo";
 const base_url = "https://platzi-avo.vercel.app/";
 
-const appNode = document.querySelector('#app');
+const appNode = document.querySelector("#app");
+
+const formatPrice = (price) => {
+  const newPrice = new window.Intl.NumberFormat("en-EN", {
+    style: "currency",
+    currency: "USD",
+  }).format(price);
+
+  return newPrice;
+};
 
 window
   .fetch(url)
@@ -21,11 +30,16 @@ window
       //crear titulo
       const title = document.createElement("h2");
       title.textContent = item.name;
+      title.style = "font-size: 2rem;";
+      title.className = "decoration-slate-900 text-red-600";
       //crear preio
       const price = document.createElement("div");
-      price.textContent = item.price;
+      price.style = "font-size: 1.2rem;font-weight: bold;";
+      price.textContent = formatPrice(item.price);
 
       const container = document.createElement("div");
+      
+      container.className = "box-content h-55 w-42 p-4 border-4 my-9";
 
       container.append(image, title, price);
 
